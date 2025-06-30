7ba87dba6807b3040699570b6d5b9b3fcaaa3135
valenneka <strxpensupp0rt@gmail.com>
2025-06-30 16:47:17 -0300

    add: Footer, navbar y iniciación de LandingPage

--- src/vista/components/footer.php
+++ src/vista/components/footer.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Footer</title>
     <link rel="stylesheet" href="../../css/footer.css">
 </head>
+
 <body>
-    
+    <footer class="footer">
+        <div class="footer_container">
+            <div class="footer_branding">
+                <img src="images/Logo.svg" alt="Logo Pizzería">
+                <span>&copy; 2025 Pizzería Dominico</span>
+            </div>
+            <div class="footer_social">
+                <a href="#" class="footer_social-link"><img src="images/instagram.svg" alt="Instagram" class="instagram"></a>
+                <a href="#" class="footer_social-link"><img src="images/facebook.svg" alt="Facebook" class="facebook"></a>
+            </div>
+        </div>
+    </footer>
 </body>
+
+
 </html>
